96baa836de8182e1f06fbc1b04d7379f48f2cfb2
TheHellFireo <ussfkamurof55@gmail.com>
2026-06-04 00:44:39 +0300

    Make corpsman trauma treatment instant

--- Content.Server/_CMU14/Medical/Wounds/CMUBandageInterceptionSystem.cs
+++ Content.Server/_CMU14/Medical/Wounds/CMUBandageInterceptionSystem.cs
@@ -91,7 +91,7 @@ public sealed class CMUBandageInterceptionSystem : EntitySystem
         }
 
         if (woundTarget != null
-            && treater.InstantWoundTreatment
+            && CanApplyInstantWoundTreatment(args.User, treater)
             && TryApplyInstantWoundTreatment(args.User, patient, targetPart, used, treater))
         {
             args.Handled = true;
@@ -446,6 +446,13 @@ public sealed class CMUBandageInterceptionSystem : EntitySystem
         }
 
         return worst is { } w ? WoundSizeProfile.BandageDelay(w) : TreatDelay;
+    }
+
+    private bool CanApplyInstantWoundTreatment(EntityUid user, WoundTreaterComponent treater)
+    {
+        return treater.InstantWoundTreatment ||
+               (treater.InstantWoundTreatmentSkills.Count > 0 &&
+                _skills.HasAllSkills(user, treater.InstantWoundTreatmentSkills));
     }
 
     private void OnBandageDoAfter(Entity<CMUBandagePendingComponent> ent, ref CMUBandageDoAfterEvent args)
