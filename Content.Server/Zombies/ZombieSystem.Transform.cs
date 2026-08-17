11591b62f9467d234b1355ff7b3ef780389de2ee
GitHubUser53123 <110841413+GitHubUser53123@users.noreply.github.com>
2026-08-17 13:10:14 +0400

    fix 2

--- Content.Server/Zombies/ZombieSystem.Transform.cs
+++ Content.Server/Zombies/ZombieSystem.Transform.cs
@@ -37,6 +37,7 @@ using Content.Shared.Traits.Assorted;
 using Robust.Shared.Audio.Systems;
 using Content.Shared.Ghost.Roles.Components;
 using Content.Shared.Tag;
+using Content.Shared._RMC14.Damage; //RNMC 14
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Content.Shared.NPC.Prototypes;
