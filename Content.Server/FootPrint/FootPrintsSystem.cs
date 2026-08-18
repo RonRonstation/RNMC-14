aff7e36b7a879fd992615c6fd1edad806975058e
GitHubUser53123 <110841413+GitHubUser53123@users.noreply.github.com>
2026-08-19 00:45:17 +0400

    fix

--- Content.Server/FootPrint/FootPrintsSystem.cs
+++ Content.Server/FootPrint/FootPrintsSystem.cs
@@ -16,6 +16,7 @@ public sealed class FootPrintsSystem : EntitySystem
 {
     [Dependency] private DecalSystem _decals = default!;
     [Dependency] private SharedMapSystem _map = default!;
+    [Dependency] private IMapManager _mapMan = default!;
     [Dependency] private IRobustRandom _random = default!;
     [Dependency] private SharedTransformSystem _transform = default!;
     [Dependency] private SharedXenoWeedsSystem _weeds = default!;
@@ -58,7 +59,7 @@ public sealed class FootPrintsSystem : EntitySystem
         if (component.PrintsColor.A <= 0f
             || !_transformQuery.TryComp(uid, out var transform)
             || !_mobThresholdQuery.TryComp(uid, out var mobThreshHolds)
-            || !_map.TryFindGridAt(_transform.GetMapCoordinates((uid, transform)), out var gridUid, out _))
+            || !_mapMan.TryFindGridAt(_transform.GetMapCoordinates((uid, transform)), out var gridUid, out _))
             return;
 
         var dragging = mobThreshHolds.CurrentThresholdState is MobState.Critical or MobState.Dead;
